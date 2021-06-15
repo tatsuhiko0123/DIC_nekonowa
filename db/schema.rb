176590ff854421430fffa818489ce68f5c27af14
tatsuhiko0123 <tatsuhiko.t.123@gmail.com>
2021-06-16 02:42:50 +0900

    postsテーブル、カラム名変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_15_152743) do
+ActiveRecord::Schema.define(version: 2021_06_15_174026) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 2021_06_15_152743) do
     t.text "breed"
     t.integer "gender"
     t.integer "age"
-    t.integer "prefectures"
+    t.integer "prefecture"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
