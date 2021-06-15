f97146786c9eb4c37a97ae6f0ea52c44212357c5
tatsuhiko0123 <tatsuhiko.t.123@gmail.com>
2021-06-16 00:33:52 +0900

    コントローラー編集

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,16 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
   def index
+    @posts = Post.all
   end
 
   def new
+    @post = Post.new
+  end
+
+  def create
+    Post.create(post_params)
+    redirect_to posts_path, notice: "新規投稿を作成しました！"
   end
 
   def show
@@ -11,6 +19,27 @@ class PostsController < ApplicationController
   def edit
   end
 
+  def update
+    if @post.update(post_params)
+      redirect_to posts_path, notice: "投稿を編集しました！"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to posts_path, notice:"投稿を削除しました！"
+  end
+
   def confirm
+  end
+  private
+  def post_params
+    params.require(:post).permit(:comment)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
