ec57820087a070a1a5d99a6ab96f62fd3fe031d5
tatsuhiko0123 <tatsuhiko.t.123@gmail.com>
2021-06-17 20:51:39 +0900

    中間テーブル、アソシエーション定義

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,8 @@
 class Post < ApplicationRecord
   mount_uploader :image, ImageUploader
   belongs_to :user
+  has_many :favorites, dependent: :destroy
+  has_many :favorite_users, through: :favorites, source: :user
 
   enum prefecture:{
      "---":0,
