112a8d7ba7e24e3f6e5f2dd8bd5081ac23bf8757
tatsuhiko0123 <tatsuhiko.t.123@gmail.com>
2021-06-19 00:05:03 +0900

    詳細情報の編集機能作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  get 'other_posts/new'
-  get 'other_posts/edit'
-  get 'other_posts/confirm'
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+
   devise_for :users
+  
   resources :users, only: [:show]
   root 'posts#index'
   resources :posts do
@@ -11,9 +10,10 @@ Rails.application.routes.draw do
       post :confirm
     end
   end
+  resources :favorites, only: [:create, :destroy]
+  resources :other_posts, only: [:edit, :update]
+
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
-  resources :favorites, only: [:create, :destroy]
-  resources :other_posts
 end
