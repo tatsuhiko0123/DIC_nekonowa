d7d0b5a8e4807356a70505758712030427c7cfb1
tatsuhiko0123 <tatsuhiko.t.123@gmail.com>
2021-06-18 16:30:30 +0900

    postテーブル・other_postテーブルのアソシエーション設定

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
   mount_uploader :image, ImageUploader
   belongs_to :user
+  has_one :other_post, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :favorite_users, through: :favorites, source: :user
 
