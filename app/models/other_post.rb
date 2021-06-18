eb8cd33532acd288706bff1badf7b8f9fcb0d619
tatsuhiko0123 <tatsuhiko.t.123@gmail.com>
2021-06-18 16:03:20 +0900

    詳細情報の新規作成・編集機能実装

--- app/models/other_post.rb
+++ app/models/other_post.rb
@@ -1,3 +1,8 @@
 class OtherPost < ApplicationRecord
   belongs_to :post
+
+  enum vaccination:{ "---":0,未接種:1,接種済:2 }, _prefix: true
+  enum operation:{ "---":0,未手術:1,手術済:2 }, _prefix: true
+  enum single:{ "---":0,可:1,不可:2 }, _prefix: true
+  enum old_age:{ "---":0,可:1,不可:2 }, _prefix: true
 end
