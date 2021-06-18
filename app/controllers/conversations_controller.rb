aa7a255d2419d343348722f927642c1f094380cc
tatsuhiko0123 <tatsuhiko.t.123@gmail.com>
2021-06-19 00:42:14 +0900

    メッセージコントローラー作成

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,2 +1,26 @@
 class ConversationsController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @conversations = Conversation.all
+  end
+
+  def create
+    if logged_in?
+      if Conversation.between(params[:sender_id], params[:recipient_id]).present?
+        @conversation = Conversation.between(params[:sender_id], params[:recipient_id]).first
+      else
+        @conversation = Conversation.create!(conversation_params)
+      end
+      redirect_to conversation_messages_path(@conversation)
+    else  
+      redirect_to root_path
+    end  
+  end
+
+  private
+  
+  def conversation_params
+    params.permit(:sender_id, :recipient_id)
+  end
 end
