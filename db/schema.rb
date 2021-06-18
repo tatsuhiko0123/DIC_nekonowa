948d718ad350a3feedbdf8a9ca708e1ea6e670c1
tatsuhiko0123 <tatsuhiko.t.123@gmail.com>
2021-06-19 00:45:57 +0900

    Conversationモデル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_18_002416) do
+ActiveRecord::Schema.define(version: 2021_06_18_154325) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "conversations", force: :cascade do |t|
+    t.integer "sender_id"
+    t.integer "recipient_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["recipient_id"], name: "index_conversations_on_recipient_id"
+    t.index ["sender_id", "recipient_id"], name: "index_conversations_on_sender_id_and_recipient_id", unique: true
+    t.index ["sender_id"], name: "index_conversations_on_sender_id"
+  end
 
   create_table "favorites", force: :cascade do |t|
     t.bigint "user_id"
