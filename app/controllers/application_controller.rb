c09cb44842a1b8add2fc6da60e166c568ab48ee6
tatsuhiko0123 <tatsuhiko.t.123@gmail.com>
2021-06-22 01:57:29 +0900

    navbar編集

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,8 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :image, :comment])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name, :image, :comment])
   end
+
+  def after_sign_out_path_for(resource)
+    users_path # ログアウト後に遷移するpathを設定
+  end
 end
