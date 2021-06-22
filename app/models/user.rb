2e9c484c51d7a65d2c23294afe6e3c0f0ac2d0ad
tatsuhiko0123 <tatsuhiko.t.123@gmail.com>
2021-06-23 02:55:57 +0900

    ユーザー編集時のパスワード入力を削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,8 +9,6 @@ class User < ApplicationRecord
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   before_validation { email.downcase! }
-  has_secure_password
-  validates :password, length: { minimum: 6 }
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
@@ -21,8 +19,6 @@ class User < ApplicationRecord
   def self.guest
     find_or_create_by!(email: 'guest@example.com') do |user|
       user.password = SecureRandom.urlsafe_base64
-      # user.confirmed_at = Time.now  # Confirmable を使用している場合は必要
-      # 例えば name を入力必須としているならば， user.name = "ゲスト" なども必要
     end
   end
 end
