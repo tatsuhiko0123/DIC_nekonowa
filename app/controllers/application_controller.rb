7ed5916f391e60075ea9c3db39f8ee73be163744
tatsuhiko0123 <tatsuhiko.t.123@gmail.com>
2021-06-23 01:43:44 +0900

    ログイン後の遷移先の変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,10 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:account_update, keys: [:name, :image, :comment])
   end
 
+  def after_sign_in_path_for(resource)
+    posts_path
+  end
+
   def after_sign_out_path_for(resource)
     users_path # ログアウト後に遷移するpathを設定
   end
