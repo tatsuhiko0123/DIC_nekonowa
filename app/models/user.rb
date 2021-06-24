5b3677b591913bb4cef4253dc7b5471e881d5940
tatsuhiko0123 <tatsuhiko.t.123@gmail.com>
2021-06-24 09:36:19 +0900

    SEEDデータ再修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :favorite_posts, through: :favorites, source: :post
 
-  validates :name, length: { maximum: 30 }
+  validates :name, presence: true, length: { maximum: 30 }
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   before_validation { email.downcase! }
