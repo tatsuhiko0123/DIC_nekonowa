1fcdc11c8c76f12937f5d1fee71d139b5820cbb4
tatsuhiko0123 <tatsuhiko.t.123@gmail.com>
2021-06-24 16:45:41 +0900

    一般ユーザーの場合に管理画面へのリンクが出ないように修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit]
-  before_action :authenticate_user!
+  skip_before_action :authenticate_user!, only: [:index]
 
   def index
   end
